c297d5e72b5c4091152b338566ed9af870a0129b
bukton123 <buk_ton2@hotmail.com>
2022-01-01 23:24:57 +0700

    Add pkg env

---
+++ pkg/env/env.go
@@ -0,0 +1,42 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
+
+func GetEnvString(key string, defaultVal string) string {
+	return getEnv(key, defaultVal)
+}
+
+func GetEnvInt(key string, defaultVal int) int {
+	valueStr := getEnv(key, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+	return defaultVal
+}
+
+func GetEnvFloat(key string, defaultVal float64) float64 {
+	valueStr := getEnv(key, "")
+	if value, err := strconv.ParseFloat(valueStr, 64); err == nil {
+		return value
+	}
+	return defaultVal
+}
+
+func GetEnvBool(key string, defaultVal bool) bool {
+	valueStr := getEnv(key, "")
+	if value, err := strconv.ParseBool(valueStr); err == nil {
+		return value
+	}
+	return defaultVal
+}
