97122b3b447593e4371865c6571ac6e210986433
bukton123 <buk_ton2@hotmail.com>
2022-01-01 23:37:11 +0700

    move handler logging to api

--- pkg/api/handler_error.go
+++ pkg/api/handler_error.go
@@ -2,9 +2,32 @@ package api
 
 import (
 	"api/pkg/logging"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"net/http"
 )
+
+type (
+	HandlerError struct {
+		Error handlerErrorMessage `json:"error"`
+	}
+
+	handlerErrorMessage struct {
+		Type    string `json:"type"`
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}
+)
+
+func HandlerErrorHttp(service string, code int, message string) HandlerError {
+	return HandlerError{
+		Error: handlerErrorMessage{
+			Type:    fmt.Sprintf("%sException", service),
+			Code:    code,
+			Message: message,
+		},
+	}
+}
 
 func ErrorHandlerFiber(service string, version string, dev bool) fiber.ErrorHandler {
 	return func(ctx *fiber.Ctx, err error) error {
@@ -19,6 +42,6 @@ func ErrorHandlerFiber(service string, version string, dev bool) fiber.ErrorHand
 		}
 
 		logging.Error(service, version, err.Error())
-		return ctx.Status(code).JSON(logging.HandlerErrorHttp(service, 0, text))
+		return ctx.Status(code).JSON(HandlerErrorHttp(service, 0, text))
 	}
 }
